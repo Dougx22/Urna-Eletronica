f96d9e955e55d8b6cf0c26cd40a030276a754fe8
Douglas Bessa <95510711+Dougx22@users.noreply.github.com>
2022-03-15 06:51:35 -0300

    Commit inicial

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '77000',
+        nome: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [{ url: 'avatar77000.png', legenda: 'Vereador' }]
+      },
+      {
+        numero: '70190',
+        nome: 'Beltrano da Silva',
+        partido: 'DEFG',
+        fotos: [{ url: 'avatar70190.jpg', legenda: 'Vereador' }]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'Ciclano',
+        partido: 'ABC',
+        Vice: 'Cic',
+        fotos: [
+          { url: 'avatar23.jpg', legenda: 'Prefeito' },
+          { url: 'avatar23_1.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        numero: '84',
+        nome: 'Zulano',
+        partido: 'QWERTY',
+        Vice: 'Zul',
+        fotos: [
+          { url: 'avatar55.jpg', legenda: 'Prefeito' },
+          { url: 'avatar55_1.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      }
+    ]
+  }
+]
